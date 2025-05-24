4ea5838f3bd50d3f1b5ed0709ec6c989875bcfaa
Clovis Fritzen <clovisf@gmail.com>
2025-05-24 14:01:46 -0300

    edited code

--- memory-game-1st-try/memory-game-1st-try.ino
+++ memory-game-1st-try/memory-game-1st-try.ino
@@ -8,8 +8,19 @@
 #define bluebutton D6
 #define yellowbutton D7
 
+bool defineSequence = true; // Whether to pick lights sequence or not (at the beginning of the game)
+int level = 1;              // Game level control variable
+char state[] = "play";      // "won", "lost", etc.
+int leds[4] = {redled, greenled, blueled, yellowled};
+int buttons[4] = {redbutton, greenbutton, bluebutton, yellowbutton};
+bool buttonState[4];
+
+struct GameResult {
+  bool defineSequence;
+  int level;
+};
+
 void setup() {
-  // put your setup code here, to run once:
   pinMode(redled, OUTPUT);
   pinMode(greenled, OUTPUT);
   pinMode(blueled, OUTPUT);
@@ -18,48 +29,90 @@ void setup() {
   pinMode(greenbutton, INPUT);
   pinMode(bluebutton, INPUT);
   pinMode(yellowbutton, INPUT);
+
+  Serial.begin(115200);
 }
+
+// 🧠 Declare your functions BEFORE using them
+GameResult gameState(bool defineSequence, int level, char* state);
+void turnLedsOn(bool defineSequence, int level, int leds[]);
+void readButtons(int buttons[], bool buttonState[]);
 
 void loop() {
-  bool defineSequence; // Whether to pick lights sequence or not (at the beginning of the game)
-  int level; // game level control variable (1-10?)
-  char state; // Char with text "won", "lost", etc
-  int leds[4]= {redled, greenled, blueled, yellowled};
-  int buttons[4]= {redbutton, greenbutton, bluebutton, yellowbutton};
+  delay(1000);
+  GameResult result = gameState(defineSequence, level, state);
+  defineSequence = result.defineSequence;
+  level = result.level;
 
-  gameState(defineSequence, level, state);
-  turnLedsOn(defineSequence, level); // show me the lights
-  readButtons(buttons); // keep reading buttons searching for user sequence
-
+  delay(1000);
+  turnLedsOn(defineSequence, level, leds); // ✅ No brackets like leds[]
+  delay(1000);
+  readButtons(buttons, buttonState);       // ✅ Same here
 }
-// create lights sequence
-void turnLedsOn(bool defineSequence, int level){
-  int sequence[10];
-  if(defineSequence == true){
-    for(int i = 0; i == level; i++){
-      sequence[i]= random(1, 4);
-    }
-    defineSequence= false; // once sequence of blinks is defined, does not enter here anymore
+
+// 💡 Create lights sequence
+void turnLedsOn(bool defineSequence, int level, int leds[]) {
+  static int sequence[10];
+  static long ledTime = 0;
+  static bool anotherHalf = false;
+  static int sequenceControl = 0;
+
+  if (level == 1) {
+    defineSequence = true;
+    anotherHalf = true;
   }
 
-}
-void readButtons(int buttons[], bool buttonsState[]){
-  bool buttonsState[4];
-  long delayTime;
+  if (defineSequence == true) {
+    for (int i = 0; i < level; i++) {
+      sequence[i] = random(0, 4);
+    }
+    defineSequence = false;
+  }
 
-  if(millis() - delayTime > 400){ // button debounce
-    for(int j= 0; j== 4; j++){
-      if(digitalRead(buttons[j]) == 1){
-        buttonsState[j]= true;
-        delayTime= millis();
+  if (millis() - ledTime > 200) {
+    if (anotherHalf == false) {
+      ledTime = millis();
+      digitalWrite(leds[sequence[sequenceControl]], HIGH);
+      Serial.println("LED ON");
+      anotherHalf = true;
+    } else {
+      digitalWrite(leds[sequence[sequenceControl]], LOW);
+      sequenceControl++;
+      if (sequenceControl >= level) {
+        sequenceControl = 0;
+       Serial.println("Reset of sequenceControl");
+      }
+      anotherHalf = false;
+    }
+  }
+}
+
+void readButtons(int buttons[], bool buttonsState[]) {
+  static long delayTime = 0;
+
+  if (millis() - delayTime > 400) {
+    for (int j = 0; j < 4; j++) {  // ✅ Use `< 4` not `== 4`
+      if (digitalRead(buttons[j]) == HIGH) {
+        buttonsState[j] = true;
         break;
       }
     }
+    delayTime = millis();
   }
-  
-   
 }
-void gameState(bool defineSequence, int level, char state){
 
+GameResult gameState(bool defineSequence, int level, char* state) {
+  static bool defineLevel = true;
 
+  if (level == 1 && strcmp(state, "lost") == 0 && defineLevel == true) {
+    level = 1;
+  } else if (strcmp(state, "lost") != 0 && defineLevel == true) {
+    level++;
+  } else if (strcmp(state, "lost") == 0) {
+    level = 1;
+    defineSequence = true;
+  }
+
+  GameResult result = {defineSequence, level};
+  return result;
 }
